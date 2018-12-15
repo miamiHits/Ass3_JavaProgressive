9ed019ba91771a8efd4c58ded3b401dca218193a
Yarden Chen <yardench@gmail.com>
2018-12-15 15:14:40 +0200

    DONE with 4

--- Ass13/src/main/java/FourInArowPCK/FourInArow.java
+++ Ass13/src/main/java/FourInArowPCK/FourInArow.java
@@ -3,38 +3,25 @@ package FourInArowPCK;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.*;
 
-//Creating class FourInArowPCK.FourInArow which extends JPanel which is what the game will run on.
-//Implements MouseMotionListener for mouse movements, MouseListener for mouse clicks, and Runnable to run the game. 
 public class FourInArow extends JFrame{
-    // Creating object JFrame to create the window for the game application.
-    // Creating object Thread for the speed at which the token shows up.
+    JButton clearButton = new JButton("Clear");
+    JButton btn1 = new JButton("1");
+    JButton btn2 = new JButton("2");
+    JButton btn3 = new JButton("3");
+    JButton btn4 = new JButton("4");
+    JButton btn5 = new JButton("5");
+    JButton btn6 = new JButton("6");
+    JButton btn7 = new JButton("7");
+    JPanel buttonsPanel = new JPanel();
+    GamePanel game = new GamePanel();
 
-
-    public static void main(String[] args) {
-        // Creating new Connect4 object.
-        new FourInArow();
-
-
-    }
 
     public FourInArow() {
         super ("WELCOME 2 four in a row game !!!");
-        JButton clearButton = new JButton("Clear");
-        JButton btn1 = new JButton("1");
-        JButton btn2 = new JButton("2");
-        JButton btn3 = new JButton("3");
-        JButton btn4 = new JButton("4");
-        JButton btn5 = new JButton("5");
-        JButton btn6 = new JButton("6");
-        JButton btn7 = new JButton("7");
-        JPanel buttonsPanel = new JPanel();
-        GamePanel game = new GamePanel();
-        setTitle("WELCOME 2 four in a row game !!!");
-        setVisible(true);
-        setSize(game.fWidth, game.fHeight);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         buttonsPanel.add(btn1);
         buttonsPanel.add(btn2);
@@ -46,7 +33,50 @@ public class FourInArow extends JFrame{
         buttonsPanel.add(clearButton);
         add(buttonsPanel, BorderLayout.PAGE_END);
         add(game, BorderLayout.CENTER);
-
+        setVisible(true);
+        setSize(game.getBOARD_WIDTH(), game.getBOARD_HEIGHT());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                handleButtonClicked(e.getY());
+            }
+        });
+        btn1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleButtonClicked(0);
+            }
+        } );
+        btn2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleButtonClicked(1);
+            }
+        } );
+        btn3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleButtonClicked(2);
+            }
+        } );
+        btn4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleButtonClicked(3);
+            }
+        } );
+        btn5.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleButtonClicked(4);
+            }
+        } );
+        btn6.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleButtonClicked(5);
+            }
+        } );
+        btn7.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleButtonClicked(6);
+            }
+        } );
         clearButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 clearGame();
@@ -54,15 +84,29 @@ public class FourInArow extends JFrame{
         } );
 
         while (true) {
-            game.fWidth = getWidth();
-            game.fHeight = getHeight();
-            game.width = game.fWidth / (game.nW + 1);
-            game.height = game.fHeight / (game.nH + 1);
+            game.setBOARD_WIDTH(getWidth());
+            game.setBOARD_HEIGHT(getHeight());
+            game.setWidth(game.getBOARD_WIDTH() / (game.getROWS() + 1));
+            game.setHeight(game.getBOARD_HEIGHT() / (game.getCOLS() + 1));
             game.repaint();
         }
     }
 
+    private void handleButtonClicked(int i) {
+        game.mgX = i;
+        game.mgY = 0;
+        game.calcCell();
+    }
+
     private void clearGame() {
+        btn1.setEnabled(true);
+        btn2.setEnabled(true);
+        btn3.setEnabled(true);
+        btn4.setEnabled(true);
+        btn5.setEnabled(true);
+        btn6.setEnabled(true);
+        btn7.setEnabled(true);
+        game.clearBoard();
 
     }
 
---
+++ Ass13/src/main/java/FourInArowPCK/Main.java
@@ -0,0 +1,8 @@
+package FourInArowPCK;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating new four in a row object.
+        new FourInArow();
+    }
+}
